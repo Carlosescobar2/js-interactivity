1cb450cafc94e050985b9fb2af755e67a4300133
Carlos Escobar <escobarcarlos@gmail.com>
2021-08-05 19:19:23 -0600

    Movie-list-done

---
+++ index.js
@@ -0,0 +1,52 @@
+let message = document.querySelector("#message")
+
+
+
+
+
+
+function addMovie(event) {
+    event.preventDefault()
+    let inputField = document.querySelector("input")
+    
+    let movie = document.createElement("li")
+    let movieTitle = document.createElement("span")
+    movieTitle.innerHTML = inputField.value
+    movieTitle.addEventListener("click", crossOffMovie)
+
+    movie.appendChild(movieTitle)
+
+    let deleteBtn = document.createElement('button')
+    deleteBtn.textContent = "X"
+    deleteBtn.addEventListener("click", deleteMovie)
+
+    movie.appendChild(deleteBtn)
+
+    let list = document.querySelector("ul")
+    list.appendChild(movie)
+    inputField.value = ""
+}
+
+let form = document.querySelector("form")
+form.addEventListener("submit", addMovie)
+
+function deleteMovie(event) {
+    event.target.parentNode.remove() 
+    message.textContent = event.target.parentNode.firstChild.textContent + " removed"
+    revealMessage()
+}
+
+function crossOffMovie(event) {
+    event.target.classList.toggle("checked")
+    if (event.target.classList.contains("checked")) {
+        message.textContent = event.target.textContent + " watched!"
+    } else {
+        message.textContent = event.target.textContent + " added back!"
+    }
+    revealMessage()
+}
+
+function revealMessage() {
+    message.classList.remove("hide");
+    setTimeout( () => message.classList.add("hide"), 1000);
+}
